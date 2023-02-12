a8b8cb1fe325a0c0255a2190451f8f40d3334505
Patricio Sanchez Vuichard <patriciosanchezvuichard@gmail.com>
2023-02-12 17:25:51 +0100

    lab done, con asi todo el bonus

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const app = require("../app")
+const MoviesModel = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
 router.get("/movies", (req, res, next)=>{
-  
-  // Crear la vista y probar renderizarla
-
-  // Buscar la data en la DB
   MoviesModel.find()
   .then((response)=>{
+    // console.log(response)
     res.render("movies.hbs", {
       allMovies: response
+    })
+  })
+  .catch((err)=>{
+    next(err)
+  })
+})
+
+router.get("/movies/:id", (req, res, next)=>{
+  const {id} = req.params
+  MoviesModel.findById(id)
+  .then((response)=>{
+    // console.log(response)
+    res.render("details.hbs", {
+      oneMovie: response
     })
   })
   .catch((err)=>{
